92cd09d449187ca2a26df5610d0d5a3931c11939
VictorArlindo <pereiravictor94@gmail.com>
2021-12-05 13:10:30 -0300

    criacao de calculadora

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -5,12 +5,13 @@ import br.com.dio.model.Gato;
 public class PrimeiroPrograma {
 
 	public static void main(String[] args) {
+	
 		
 		Gato gato = new Gato();
 		Livros livros = new Livros();
 		
 		System.out.println(gato);
-		System.out.println(livros);
+		System.out.println();
 		
 		/*int a = 2;
 		int b = 3;
